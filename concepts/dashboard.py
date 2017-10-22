dee99a1b3e69a4e9e077dffe35921a2517bc8aa4
Mochan <mochan.shrestha@gmail.com>
2017-10-21 21:15:42 -0400

    start on concepts

---
+++ concepts/dashboard.py
@@ -0,0 +1,5 @@
+
+# Get data from different processes, compose them and show the final dashboard in a pygame window.
+# Also get user input and then send it back if needed.
+
+def Dashboard (frame):
